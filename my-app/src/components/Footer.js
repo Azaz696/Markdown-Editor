c08b055f5aa38564831f0785c0a49c814049be73
Azaz Ali <aliazaz7296@gmail.com>
2023-04-02 22:13:01 +0530

    markdown added

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>
+          <a href="https://github.com/Azaz696/">Developed by Azaz 🧡</a>
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
